f20f402c42bc00ec68bfdad8ce150c87ee6b6606
Rajatsinha05 <rajatsinha5467@gmail.com>
2023-03-25 16:18:57 +0530

    api testing

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
-const express=require('express');
-
+const express = require("express");
 
 const app = express();
 
+let arr = [
+  { name: "John", age: 34, id: 1 },
+  { name: "John", age: 34, id: 2 },
+  { name: "John", age: 34, id: 3 },
+  { name: "John", age: 34, id: 4 },
+  { name: "John", age: 34, id: 5 },
+];
 
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send(arr);
+});
 
-app.get("/",(req,res)=>{
+app.post("/add", (req, res) => {
+  console.log(req.body);
+  arr.push(req.body);
 
-    res.send("working")
-})
+  res.status(200).send(arr);
+});
 
+app.delete("/delete/:id", (req, res) => {
+  let { id } = req.params;
 
+  console.log(id);
 
-app.listen(8080,()=>{
-    console.log("server is runnning on port 8080")
-})
+  let data = arr.filter((item) => item.id != id);
+
+  res.status(200).send(data);
+});
+
+app.patch("/update/:id", (req, res) => {
+  let { id } = req.params;
+
+  let { name } = req.body;
+  console.log(name);
+
+  let data = arr.map((item) => {
+    if (item.id == id) {
+      item.name = name;
+      res.send(item);
+    }
+  });
+
+  res.send(data);
+});
+
+app.listen(8081, () => {
+  console.log("server is runnning on port 8080");
+});
